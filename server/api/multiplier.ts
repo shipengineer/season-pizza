7f06357538d0594ece6e0b18148961312ef3f706
mav-design <mav.dev.design@gmail.com>
2024-04-11 13:25:15 +0300

    add pwa

---
+++ server/api/multiplier.ts
@@ -0,0 +1,8 @@
+export default defineEventHandler((event) => {
+    return [
+     {name:25,value:1},
+     {name:30, value:2},
+     {name:35,value:3}
+    ];
+  });
+  
---
+++ server/api/toppings.ts
@@ -0,0 +1,15 @@
+export default defineEventHandler((event) => {
+  return [
+    { name: 'моцарелла', price: 55, img: 'mozzarella.jpg' },
+    { name: 'пепперони', price: 55, img: 'pepperoni.jfif' },
+    { name: 'черри', price: 55, img: 'cherry.jpg' },
+    { name: 'бекон', price: 55, img: 'beacon.webp' },
+    { name: 'ветчина', price: 45, img: 'ham.jpg' },
+    { name: 'шампиньоны', price: 45, img: 'mushrooms.webp' },
+    { name: 'халапеньо', price: 55, img: 'jalapeno.jfif' },
+    { name: 'соленые огурчики', price: 55, img: 'pickle.webp' },
+    { name: 'дор блю', price: 65, img: 'dorblu.jpeg' },
+    { name: 'креветки', price: 95, img: 'shrimps.jfif' },
+    { name: 'ананасы', price: 55, img: 'pineapple.jfif' },
+  ];
+});
