eb7d2e7da12efd25c44c2bd0f9b3d4617670de6e
mav-design <mav.dev.design@gmail.com>
2024-04-11 13:25:05 +0300

    add pwa

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  modules: ['@pinia/nuxt', '@nuxtjs/pwa'],
+  devtools: { enabled: true },
+  pwa: {
+    icon: {
+
+    }
+  }
 })
